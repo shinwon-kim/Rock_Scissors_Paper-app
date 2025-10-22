65d24a217e5f2f49636af2e2660ae1761f833278
shinwonkim <shinwonkkim@gmail.com>
2025-10-23 00:04:49 +0900

    feat: Add logic to determine the game winner

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let playerScore = 0;
 let computerScore = 0;
 let moves = 0;
-const totlaCount = 10;
+const totalCount = 10;
 
 const playGame = () => {
     const rockBtn = document.querySelector(".rock");
-    const paperBtn = document.querySelector(".papaer");
-    const sissorBtn = document.querySelector(".sissor");
+    const paperBtn = document.querySelector(".paper");
+    const scissorBtn = document.querySelector(".scissors");
 
-    const playerOptions = [rockBtn, sissorBtn, paperBtn];
-    const computerOptions = ["rock", "paper", "sissors"];
+    const playerOptions = [rockBtn, paperBtn, scissorBtn];
+    const computerOptions = ["ROCK", "PAPER", "SCISSORS"];
 
     playerOptions.forEach(option => {
         option.addEventListener("click", () => {
             const movesLeft = document.querySelector(".movesleft");
             moves++;
-            movesLeft.innerHTML = `Remaining Attempts: ${totlaCount - moves}`;
+            movesLeft.innerHTML = `Remaining Attempts: ${totalCount - moves}`;
 
             const choiceNumber = Math.floor(Math.random() * 3);
             const computerChoice = computerOptions[choiceNumber];
-
+            
             // Check who wins
             winner(option.innerText, computerChoice)
         })
@@ -27,7 +27,43 @@ const playGame = () => {
 }
 
 const winner = (player, computer) => {
-
+    const result = document.querySelector(".result");
+    const playerScoreBoard = document.querySelector(".p-count");
+    const computerScoreBoard = document.querySelector(".c-count");
+    console.log(player, computer);
+    if(player === computer){
+        result.textContent = "DRAW";
+    } else if (player === "ROCK") {
+        if (computer === "PAPER") {
+            result.textContent = "COMPUTER WINS";
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        } else {
+            result.textContent = "PLAYER WINS";
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    } else if (player === "SCISSORS") {
+        if (computer === "ROCK") {
+            result.textContent = "COMPUTER WINS";
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        } else {
+            result.textContent = "PLAYER WINS";
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    } else if (player === "PAPER") {
+        if (computer === "SCISSORS"){
+            result.textContent = "COMPUTER WINS";
+            computerScore++;
+            computerScoreBoard.textContent = computerScore;
+        } else {
+            result.textContent = "PLAYER WINS";
+            playerScore++;
+            playerScoreBoard.textContent = playerScore;
+        }
+    }
 }
 
 playGame();
